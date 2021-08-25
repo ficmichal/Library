8bf4c61e3748859b9b88a2ada6c46b91106ab31e
ficmichal <meho.fic@gmail.com>
2021-08-25 16:48:45 +0200

    create IBookRepository in Domain

---
+++ src/Modules/Lending/Domain/Books/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Library.Modules.Lending.Domain.Books
+{
+    public interface IBookRepository
+    {
+        Task<IBook> FindBy(BookId id);
+
+        Task Save(IBook book);
+    }
+}
