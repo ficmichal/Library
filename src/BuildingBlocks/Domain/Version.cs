4de84552de5ecd7e27bc7a081826101414074a78
ficmichal <meho.fic@gmail.com>
2021-08-25 16:42:33 +0200

    Create Version as Domain BuildingBlock

---
+++ src/BuildingBlocks/Domain/Version.cs
@@ -0,0 +1,17 @@
+﻿namespace Library.BuildingBlocks.Domain
+{
+    public class Version
+    {
+        public int Value { get; }
+
+        public static Version Zero()
+        {
+            return new(0);
+        }
+
+        private Version(int version)
+        {
+            Value = version;
+        }
+    }
+}
