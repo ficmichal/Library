4146e034dc4da49cfac73c72b5981a7f5a752121
ficmichal <meho.fic@gmail.com>
2021-08-26 11:51:51 +0200

    Create DomainEvent

---
+++ src/BuildingBlocks/Domain/Events/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Library.BuildingBlocks.Domain.Events
+{
+    public interface IDomainEvent
+    {
+        Guid EventId { get; }
+
+        Guid AggregateId { get; }
+
+        DateTime When { get; }
+    }
+}
