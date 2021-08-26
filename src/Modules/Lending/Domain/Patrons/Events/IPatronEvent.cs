c722aa84f96dd86ed91efce54ef3084bfb733895
ficmichal <meho.fic@gmail.com>
2021-08-26 11:52:09 +0200

    Create Patron Domain Event interface

---
+++ src/Modules/Lending/Domain/Patrons/Events/IPatronEvent.cs
@@ -0,0 +1,15 @@
+﻿using Library.BuildingBlocks.Domain.Events;
+using System;
+using System.Collections.Generic;
+
+namespace Library.Modules.Lending.Domain.Patrons.Events
+{
+    public interface IPatronEvent : IDomainEvent
+    {
+        PatronId PatronId => new(PatronIdValue);
+
+        Guid PatronIdValue { get; }
+
+        List<IDomainEvent> Normalize() => new() {this};
+    }
+}
