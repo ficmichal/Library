8fbc09c70f43a44e8bfcc47676ee8912792cb471
ficmichal <meho.fic@gmail.com>
2021-08-27 21:08:30 +0200

    hold VO

---
+++ src/Modules/Lending/Domain/Patrons/Hold/Hold.cs
@@ -0,0 +1,26 @@
+﻿using Library.BuildingBlocks.Domain;
+using Library.Modules.Lending.Domain.Books;
+using Library.Modules.Lending.Domain.LibraryBranch;
+using System.Collections.Generic;
+
+namespace Library.Modules.Lending.Domain.Patrons.Hold
+{
+    public class Hold : ValueObject
+    {
+        public Hold(BookId bookId, LibraryBranchId libraryBranchId)
+        {
+            BookId = bookId;
+            LibraryBranchId = libraryBranchId;
+        }
+
+        public BookId BookId { get; }
+
+        public LibraryBranchId LibraryBranchId { get; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return BookId;
+            yield return LibraryBranchId;
+        }
+    }
+}
