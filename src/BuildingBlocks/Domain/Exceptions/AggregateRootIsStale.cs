563339bc2e73a7d73c46b6288102b95ecbd868a3
ficmichal <meho.fic@gmail.com>
2021-08-31 15:18:53 +0200

    Aggregate excpetion

---
+++ src/BuildingBlocks/Domain/Exceptions/AggregateRootIsStale.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Library.BuildingBlocks.Domain.Exceptions
+{
+    public class AggregateRootIsStale : Exception
+    {
+        public AggregateRootIsStale(string message) : base(message) {}
+    }
+}
