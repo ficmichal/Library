17da0cd0d2b257b786966b7c0b865fbf9b3268b6
ficmichal <meho.fic@gmail.com>
2021-09-01 13:12:33 +0200

    PatronsDbContext

--- src/Modules/Lending/Infrastructure/Patrons/PatronsDbContext.cs
+++ src/Modules/Lending/Infrastructure/Patrons/PatronsDbContext.cs
@@ -5,5 +5,31 @@ namespace Library.Modules.Lending.Infrastructure.Patrons
     public class PatronsDbContext : DbContext
     {
         public DbSet<PatronDatabaseEntity> Patrons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PatronDatabaseEntity>()
+                .HasKey(x => x.Id);
+
+            modelBuilder.Entity<PatronDatabaseEntity>()
+                .Property(x => x.PatronId);
+
+            modelBuilder.Entity<PatronDatabaseEntity>()
+                .Property(x => x.PatronType);
+
+            modelBuilder.Entity<PatronDatabaseEntity>()
+                .OwnsMany(x => x.BooksOnHold, books =>
+                {
+                    books.HasKey(x => x.Id);
+
+                    books.Property(x => x.BookId);
+
+                    books.Property(x => x.LibraryBranchId);
+
+                    books.Property(x => x.Till);
+
+                    books.WithOwner().HasForeignKey(x => x.PatronId);
+                });
+        }
     }
 }
