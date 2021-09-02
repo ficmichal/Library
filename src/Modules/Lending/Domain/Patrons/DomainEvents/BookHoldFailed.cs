df5f955d30fdb90cd77b05a576be321cead6034d
ficmichal <meho.fic@gmail.com>
2021-09-02 12:51:11 +0200

    BookHoldFailed event

---
+++ src/Modules/Lending/Domain/Patrons/DomainEvents/BookHoldFailed.cs
@@ -0,0 +1,36 @@
+﻿using Library.BuildingBlocks.Domain.Policies;
+using Library.Modules.Lending.Domain.Books;
+using Library.Modules.Lending.Domain.LibraryBranch;
+using System;
+
+namespace Library.Modules.Lending.Domain.Patrons.DomainEvents
+{
+    public class BookHoldFailed : IPatronEvent
+    {
+        public Guid EventId => new();
+        public string Reason { get; }
+        public DateTime When { get; }
+        public Guid PatronIdValue { get; }
+        public Guid BookId { get; }
+        public Guid LibraryBranchId { get; }
+
+        public static BookHoldFailed BookHoldFailedNow(Rejection rejection, BookId bookId,
+            LibraryBranchId libraryBranchId, PatronInformation patronInformation)
+        {
+            return new(
+                rejection.Reason.Value,
+                patronInformation.PatronId.Id,
+                bookId.Id,
+                libraryBranchId.Id);
+        }
+
+        private BookHoldFailed(string reason, Guid patronId, Guid bookId, Guid libraryBranchId)
+        {
+            Reason = reason;
+            When = DateTime.Now;
+            PatronIdValue = patronId;
+            BookId = bookId;
+            LibraryBranchId = libraryBranchId;
+        }
+    }
+}
