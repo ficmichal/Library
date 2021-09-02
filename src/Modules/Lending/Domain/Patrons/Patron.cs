3277837be075a50df11129508efcf8da29b0b8e2
ficmichal <meho.fic@gmail.com>
2021-09-02 12:58:32 +0200

    PatronCanHold functionality

--- src/Modules/Lending/Domain/Patrons/Patron.cs
+++ src/Modules/Lending/Domain/Patrons/Patron.cs
@@ -1,8 +1,12 @@
 ﻿using Library.BuildingBlocks.Domain;
+using Library.BuildingBlocks.Domain.Policies;
 using Library.Modules.Lending.Domain.Books.Types;
 using Library.Modules.Lending.Domain.Patrons.DomainEvents;
 using Library.Modules.Lending.Domain.Patrons.Hold;
+using Library.Modules.Lending.Domain.Patrons.Policies;
 using System.Collections.Generic;
+using System.Linq;
+using static Library.Modules.Lending.Domain.Patrons.DomainEvents.BookHoldFailed;
 using static Library.Modules.Lending.Domain.Patrons.DomainEvents.BookPlacedOnHold;
 using static Library.Modules.Lending.Domain.Patrons.DomainEvents.BookPlacedOnHoldEvents;
 
@@ -10,15 +14,18 @@ namespace Library.Modules.Lending.Domain.Patrons
 {
     public class Patron : ValueObject
     {
-        public Patron(PatronInformation patronInformation, PatronHolds patronHolds)
+        public Patron(PatronInformation patronInformation, PatronHolds patronHolds, List<IPlacingOnHoldPolicy> placingOnHoldPolicies)
         {
             PatronInformation = patronInformation;
             PatronHolds = patronHolds;
+            _placingOnHoldPolicies = placingOnHoldPolicies;
         }
 
         public PatronInformation PatronInformation { get; }
 
         public PatronHolds PatronHolds { get; }
+
+        private readonly List<IPlacingOnHoldPolicy> _placingOnHoldPolicies;
 
         public IPatronEvent PlaceOnHold(AvailableBook book)
         {
@@ -27,6 +34,12 @@ namespace Library.Modules.Lending.Domain.Patrons
 
         public IPatronEvent PlaceOnHold(AvailableBook book, HoldDuration holdDuration)
         {
+            var rejection = PatronCanHold(book, holdDuration);
+            if (rejection is { })
+            {
+                return BookHoldFailedNow(rejection, book.Id, book.LibraryBranchId, PatronInformation);
+            }
+
             var bookPlacedOnHold = BookPlacedOnHoldNow(PatronInformation.PatronId, book.Id, book.Type,
                 book.LibraryBranchId, holdDuration);
 
@@ -34,6 +47,14 @@ namespace Library.Modules.Lending.Domain.Patrons
         }
 
         public int NumberOfHolds() => PatronHolds.Count;
+
+        private Rejection PatronCanHold(AvailableBook aBook, HoldDuration holdDuration)
+        {
+            return _placingOnHoldPolicies
+                .Select(x => x.Check(aBook, this, holdDuration))
+                .OfType<Rejection>()
+                .FirstOrDefault();
+        }
 
         protected override IEnumerable<object> GetEqualityComponents()
         {
