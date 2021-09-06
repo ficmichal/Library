f2aafc4c01f5ce74e3bf9220e711003bb9adc3ff
ficmichal <meho.fic@gmail.com>
2021-09-06 11:46:50 +0200

    scripts

---
+++ db/scripts/CreateDatabase.sql
@@ -0,0 +1,8 @@
+﻿CREATE DATABASE [Library] CONTAINMENT = NONE 
+ON PRIMARY 
+( NAME = N'Library', FILENAME = N'/var/opt/mssql/data/Library.mdf' , SIZE = 8192KB , FILEGROWTH = 65536KB ) 
+LOG ON  ( NAME = N'Library_log', FILENAME = N'/var/opt/mssql/data/Library_log.ldf' , SIZE = 8192KB , FILEGROWTH = 65536KB )
+GO
+
+USE [Library]
+GO
