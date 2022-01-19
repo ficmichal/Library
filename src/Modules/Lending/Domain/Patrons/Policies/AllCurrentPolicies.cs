766f4169364cdb526a3f63ff796a050fc9af2a46
ficmichal <meho.fic@gmail.com>
2022-01-19 23:26:02 +0100

    OnlyResearcherPatronsCanPlaceOpenEndedHoldsPolicy

--- src/Modules/Lending/Domain/Patrons/Policies/AllCurrentPolicies.cs
+++ src/Modules/Lending/Domain/Patrons/Policies/AllCurrentPolicies.cs
@@ -7,7 +7,8 @@ namespace Library.Modules.Lending.Domain.Patrons.Policies
         public static List<IPlacingOnHoldPolicy> Get()
             => new()
             {
-                new RegularPatronMaximumNumberOfHoldsPolicy()
+                new RegularPatronMaximumNumberOfHoldsPolicy(),
+                new OnlyResearcherPatronsCanPlaceOpenEndedHoldsPolicy(),
             };
     }
 }
---
+++ src/Modules/Lending/Domain/Patrons/Policies/OnlyResearcherPatronsCanPlaceOpenEndedHoldsPolicy.cs
@@ -0,0 +1,19 @@
+﻿using Library.BuildingBlocks.Domain.Policies;
+using Library.Modules.Lending.Domain.Books.Types;
+using Library.Modules.Lending.Domain.Patrons.Hold;
+
+namespace Library.Modules.Lending.Domain.Patrons.Policies
+{
+    public class OnlyResearcherPatronsCanPlaceOpenEndedHoldsPolicy : IPlacingOnHoldPolicy
+    {
+        public IPolicyResult Check(AvailableBook book, Patron patron, HoldDuration holdDuration)
+        {
+            if (patron.IsRegular() && holdDuration.IsOpenEnded())
+            {
+                return Rejection.WithReason("Regular patron cannot place open ended holds.");
+            }
+
+            return new Allowance();
+        }
+    }
+}
